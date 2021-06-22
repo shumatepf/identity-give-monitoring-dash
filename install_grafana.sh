88f679cea819eb82e54e8402abca2865294d3828
Garrett Folks <garrett.folks@gsa.gov>
2021-06-22 14:56:43 -0400

    Update grafana to v8.0.3 (#10)
    * Simplify hash-checking logic
    
    * Bump grafana version

--- install_grafana.sh
+++ install_grafana.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 set -e
 
+download_output_file="grafana.tar.gz"
+
 # To update grafana, update the download URL and checksum below.
 # New versions can be found at https://grafana.io/download/
-grafana_download_url="https://dl.grafana.com/oss/release/grafana-8.0.2.linux-amd64.tar.gz"
-grafana_sha256sum="4013062cddcc8d825cbf6bdbb3d1877eb0a2c5c57b9c7f8ec77a3afd8b5161a9  grafana.tar.gz"
+download_url="https://dl.grafana.com/oss/release/grafana-8.0.3.linux-amd64.tar.gz"
+valid_hash="db1d0584a8a7ad70057a725c503878437449341aea8b12813594fb05934d3a3f  $download_output_file"
 
 # Download the grafana archive
-download_output_file="grafana.tar.gz"
-wget --quiet --output-document "$download_output_file" "$grafana_download_url"
+wget --quiet --output-document "$download_output_file" "$download_url"
 
 # Compare sha256sum
-download_hash="$(sha256sum "$download_output_file")"
-
-if [[ "$download_hash" != "$grafana_sha256sum" ]]; then
-    echo "sha256sum failed validation"
-    exit 1
-fi
+echo "$valid_hash" | sha256sum --check --quiet
 
 # Extract the archive to the current directory, preserving the existing grafana.yml
 tar -xzf "$download_output_file" --strip-components 1 --skip-old-files

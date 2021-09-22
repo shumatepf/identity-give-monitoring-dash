bfd164cbae7c84cb2088182e98b14f1571e34856
Garrett Folks <garrett.folks@gsa.gov>
2021-09-22 11:24:35 -0400

    Bump grafana version 8.1.4 -> 8.1.5 (#26)

--- install_grafana.sh
+++ install_grafana.sh
@@ -6,8 +6,8 @@ download_output_file="grafana.tar.gz"
 # To update grafana, update the download URL and checksum below.
 # New versions can be found at https://grafana.com/grafana/download
 # in the 'Standalone Linux Binaries' section.
-download_url="https://dl.grafana.com/oss/release/grafana-8.1.4.linux-amd64.tar.gz"
-valid_hash="6b57d8e16f197c0f7049bd4388d8b14e7c0a85fa09fac6a56915dccb5636b7d6  $download_output_file"
+download_url="https://dl.grafana.com/oss/release/grafana-8.1.5.linux-amd64.tar.gz"
+valid_hash="c84ee6857de97989d3762634630289ed232fcf88979af267733d34daf7455c58  $download_output_file"
 
 # Download the grafana archive
 wget --quiet --output-document "$download_output_file" "$download_url"
